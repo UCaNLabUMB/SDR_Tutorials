fe57000ff0653537aab7d36b3bad1b2787f58c94
Michael Rahaim <michael.rahaim@umb.edu>
2024-07-30 14:39:33 -0400

    Code updates for xmplrc and zmq tests
    updated and added new flowgraph examples

--- Flowgraphs/04_RemoteControl/Scripts/02_ZMQ/ZMQ_Script_02.py
+++ Flowgraphs/04_RemoteControl/Scripts/02_ZMQ/ZMQ_Script_02.py
@@ -2,9 +2,11 @@
 #Author: UCaN Lab UMB
 #Umass Boston, MA,USA
 
-import zmq
-import time
 import numpy as np
+import xmlrpc.client
+from xmlrpc.client import ServerProxy
+import time
+import zmq
 
 #################################################################
 # Functions
@@ -28,24 +30,38 @@ def zmq_measure(my_socket,print_measurements):
 
 
 	return measurement
-	
+
 #################################################################
 # Script Configuration
 #################################################################
 PRINT_MEASUREMENTS = 0
 
+rpc_address = 'localhost'
+rpc_port = '8080'
 zmq_address = '127.0.0.1'
 zmq_port = '55555'
 
+
 # Simulation Parameters
-n_iter     = 3    # Number of iterations per scenario
-t_pause    = 2    # Delay time (sec) between iterations
+t_setup =1        # Delay time (sec) between setting and measurement
+t_pause = 2       # Delay time (sec) between iterations
 init_pause = 1    # Delay time (sec) to allow for ZMQ setup
+
+# Configuration settings from flowgraph
+REAL_SINE = 0
+COMPLEX_SINE = 1
+
+FILTER_ENABLE = 0
+FILTER_BYPASS = 1
+
 
 
 #################################################################
 # Setup
 #################################################################
+# Setup XMLRPC
+xmlrpc_control = ServerProxy('http://' + rpc_address + ':' + rpc_port)
+
 # Setup ZMQ
 context = zmq.Context()
 socket1 = context.socket(zmq.SUB)
@@ -58,14 +74,45 @@ socket1.setsockopt(zmq.SUBSCRIBE, b'')
 #################################################################
 # Execution (MAIN)
 #################################################################
-
 # Initial pause to make sure ZMQ is setup
 time.sleep(init_pause)
 
-for iter in range(n_iter):
-	print("Iteration ", iter)
-	P_avg = zmq_measure(socket1,PRINT_MEASUREMENTS)
-	print(f"  Average Power: {P_avg:.4}")
-	time.sleep(t_pause)
+# Configuration 1
+print("Real Sine, Filter Enabled: ")
+xmlrpc_control.set_path_select(REAL_SINE)       # Set signal config
+xmlrpc_control.set_filter_select(FILTER_ENABLE) # Set filter config
+time.sleep(t_pause)
+P_avg = zmq_measure(socket1,PRINT_MEASUREMENTS)
+print(f"  Average Power: {P_avg:.4}")
+time.sleep(t_pause)
 
+
+# Configuration 2
+print("Complex Sine, Filter Enabled: ")
+xmlrpc_control.set_path_select(COMPLEX_SINE)       # Set signal config
+xmlrpc_control.set_filter_select(FILTER_ENABLE) # Set filter config
+time.sleep(t_pause)
+P_avg = zmq_measure(socket1,PRINT_MEASUREMENTS)
+print(f"  Average Power: {P_avg:.4}")
+time.sleep(t_pause)
+
+
+# Configuration 3
+print("Real Sine, Filter Bypass: ")
+xmlrpc_control.set_path_select(REAL_SINE)       # Set signal config
+xmlrpc_control.set_filter_select(FILTER_BYPASS) # Set filter config
+time.sleep(t_pause)
+P_avg = zmq_measure(socket1,PRINT_MEASUREMENTS)
+print(f"  Average Power: {P_avg:.4}")
+time.sleep(t_pause)
+
+
+# Configuration 4
+print("Complex Sine, Filter Bypass: ")
+xmlrpc_control.set_path_select(COMPLEX_SINE)       # Set signal config
+xmlrpc_control.set_filter_select(FILTER_BYPASS) # Set filter config
+time.sleep(t_pause)
+P_avg = zmq_measure(socket1,PRINT_MEASUREMENTS)
+print(f"  Average Power: {P_avg:.4}")
+time.sleep(t_pause)
 
